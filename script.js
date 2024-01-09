c12d423819bead97dab47abf1f4c01c9109059ab
Giovanni Vicentin <giovannifvicentin@gmail.com>
2024-01-09 10:52:50 -0300

    Create the resolution of the first and second exercise

---
+++ script.js
@@ -0,0 +1,3 @@
+let username = prompt('What is your name?');
+
+alert('Hello, ' + username + '!');
---
+++ scriptTwo.js
@@ -0,0 +1,7 @@
+let numberOne = prompt('Enter the first number: ');
+
+let numberTwo = prompt('Enter the second number: ');
+
+let sum = Number(numberOne) + Number(numberTwo);
+
+alert('The sum is ' + sum + '.');
